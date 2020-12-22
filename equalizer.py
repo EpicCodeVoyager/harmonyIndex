0a7022d446a022b406bf2cd0eb271ee0edc98a0e
kanishkyadav47 <kanishkyadav47@gmail.com>
2020-12-22 06:39:30 +0000

    Rename file and functions

--- equalizer.py
+++ equalizer.py
@@ -12,6 +12,7 @@ def uncommon(lst1, lst2):
     return l
 
 # function that prints the countries of both dataframes that have common substring of length > 4
+# Some countries have different names (Different cases, short form names) in the indexes
 def spell_error_detect(predictor_list, covid_list):
     print(covid_list)
     print(predictor_list)
@@ -22,6 +23,7 @@ def spell_error_detect(predictor_list, covid_list):
             if len(answer) > 4:
                 print(i, ':', j)
 
+# function implementing longest common substring problem
 def longestSubstringFinder(string1, string2):
     answer = ""
     len1, len2 = len(string1), len(string2)
@@ -37,14 +39,14 @@ def longestSubstringFinder(string1, string2):
                 match = ""
     return answer
 
-# dictionary generated using output of spell_error_detect
+# dictionary generated using output of spell_error_detect()
 country_map = {'brunei darussalam' : 'brunei','czech republic' : 'czechia', 'egypt, arab rep.' : 'egypt',
                'gambia, the' : 'gambia', 'iran, islamic rep.' : 'iran', 'korea, rep.' : 's. korea',
                'kyrgyz republic' : 'kyrgyzstan', 'russian federation' : 'russia',
-               'slovak republic' : 'slovakia', 'venezuela, rb' : 'venezuela'}
+                   'slovak republic' : 'slovakia', 'venezuela, rb' : 'venezuela'}
 
 # retaining only common indexes of covid and predictor dataframe
-def df_stanardiser(pred_df, covid_df, country_map):
+def retain_common_index(pred_df, covid_df, country_map):
     # replace the indexes in predictor dataframe with the covid dataframe index value
     pred_indexes = pred_df.index.tolist()
     for key, value in country_map.items():
@@ -59,33 +61,32 @@ def df_stanardiser(pred_df, covid_df, country_map):
     return covid_df, pred_df
 
 
-def preprocess(covid_df, predictor_df):
+def equalize(covid_df, predictor_df):
 
-    # Code for matching indexes of predictor and covid dataframes
-    predictor_df.set_index('Country', inplace=True)
-    predictor_df.index = predictor_df.index.str.lower()
-    covid_df.set_index('country', inplace=True)
-    covid_df.index = covid_df.index.str.lower()
-
-    covid_df.dropna(inplace=True, subset=['deaths_per_1M']) # drop rows having na values in the column deaths_per_1M
-    #covid_df[['deaths_per_1M']] = covid_df[['deaths_per_1M']].apply(pd.to_numeric)
-    #covid_df.apply(pd.to_numeric)
+    # Drop entries from covid dataframe which have NA/missing values in the column deaths_per_1M
+    covid_df.dropna(inplace=True, subset=['deaths_per_1M'])
 
     # the numerical value are stored as strings in the dataframe, with commas
     # Removing comma and converting to numeric value
     covid_df.replace(',', '', regex=True, inplace=True)
     covid_df[covid_df.columns] = covid_df[covid_df.columns].apply(pd.to_numeric, errors='coerce')
 
+    spell_error_detect(sorted(uncommon(predictor_df.index.tolist(), covid_df.index.tolist())), sorted(uncommon(covid_df.index.tolist(), predictor_df.index.tolist())))
 
-    #covid_list = covid_df.index.tolist()
-    #predictor_list = predictor_df.index.tolist()
-    #spell_error_detect(sorted(uncommon(predictor_list, covid_list)), sorted(uncommon(covid_list, predictor_list)))
-
-    return df_stanardiser(predictor_df, covid_df, country_map)
+    return retain_common_index(predictor_df, covid_df, country_map)
 
 covid_df = pd.read_csv('./' + 'data/covid_worldometer.csv')
 predictor_df = pd.read_csv("./my_df_removedNA.csv")
-covid_df, predictor_df = preprocess(covid_df, predictor_df)
+
+# Convert indexes of predictor and covid dataframes to contain only lower case
+predictor_df.set_index('Country', inplace=True)
+predictor_df.index = predictor_df.index.str.lower()
+covid_df.set_index('country', inplace=True)
+covid_df.index = covid_df.index.str.lower()
+
+covid_df, predictor_df = equalize(covid_df, predictor_df)
+covid_df.to_csv('./'+'covid.csv')
+predictor_df.to_csv('./'+'predictor.csv')
 
 
 '''Replace corrected index in predictor df DONE
